7042156ac3cc33e54b1e2ca59ca65aed54c4c838
Monterey101 <kocic_kr@outlook.com>
2022-06-25 09:31:38 +1000

    Added CPU player
    Random number generator

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import random
+
+
 print("Welcome to Python CLI Scissors Paper Rock")
 
 humanScore = 0
 cpuScore= 0
 
 print("Current Score: Human: " , str(humanScore) , " : " , str(cpuScore) , " :CPU")
+print()
+choice = input("Choose Scissors (s), Paper (p) or Rock (r): ")
+
+def cpuChoice():
+    choice = random.choice([1, 2, 3])
+    if choice == 1:
+        return "s"
+    elif choice == 2:
+        return "p"
+    elif choice == 3:
+        return "r"
+    else:
+        return ""
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+: Human: " , str(humanScore) , " : " , str(cpuScore) , " :CPU")

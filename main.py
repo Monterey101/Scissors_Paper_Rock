de1de6fa49dc60b77af94a4553e8a339ea8e8615
Monterey101 <kocic_kr@outlook.com>
2022-06-25 09:51:16 +1000

    Creating playing interface

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import random
 #Welcome Page
 print("Welcome to Python CLI Scissors Paper Rock")
 
+#Playing Status
+playing = True
+
 #Current Human and CPU scores
 humanScore = 0
 cpuScore= 0
-
-#Playing interface
-print("Current Score: Human: " , str(humanScore) , " : " , str(cpuScore) , " :CPU")
-print()
-choice = input("Choose Scissors (s), Paper (p) or Rock (r): ")
 
 #CPU player
 def cpuChoice():
@@ -23,3 +21,47 @@ def cpuChoice():
         return "r"
     else:
         return ""
+
+#Checks if the user entered a valid choice
+def checkValidChoice(choice:str):
+    if choice == "s" or choice == "p" or choice == "r":
+        return True
+    else:
+        return False
+
+#Playing interface
+while playing == True:
+    playerChoice = input("Choose Scissors (s), Paper (p) or Rock (r): ")
+    computerChoice = cpuChoice()
+    
+    #Checks if the user entered a valid choice
+    if checkValidChoice(playerChoice):
+        
+        #Finding who won the match
+        if playerChoice == computerChoice:
+            print("Draw!")
+        elif playerChoice == "s" and computerChoice == "p":
+            humanScore += 1
+            print("You won!")
+        elif playerChoice == "p" and computerChoice == "r":
+            humanScore += 1
+            print("You won!")
+        elif playerChoice == "r" and computerChoice == "s":
+            humanScore += 1
+            print("You won!")
+        else:
+            cpuScore += 1
+            print("The Computer won!")
+        print()
+        print("Current Score: Human: " , str(humanScore) , " : " , str(cpuScore) , " :CPU")
+        print()
+    
+    #End loop if user wishes to exit
+    elif playerChoice == "e":
+        playing = False
+        print("Thanks for playing!")
+        print("Final Score: Human: " , str(humanScore) , " : " , str(cpuScore) , " :CPU")
+    
+    #Ask user to re-enter if they enter an invalid choice
+    else:
+        print("You did not enter a valid choice, try again! (enter 'e' to exit)")
